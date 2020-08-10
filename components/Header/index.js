e0c2b579695ee48ccad90de5b95983fc9d3a4266
Mark Cipolla <mark@markcipolla.com>
2020-08-10 21:29:21 +1000

    Add a lil animation on the header

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,6 +5,7 @@ const Container = styled.div`
   border-bottom: 1px solid #eee;
   padding: 20px 0;
   margin-bottom: 40px;
+  position: relative;
 `
 
 const MugShot = styled.img`
@@ -14,11 +15,31 @@ const MugShot = styled.img`
   display: block;
 `
 
+const Saw = styled.img`
+  width: 154px;
+  border: 2px solid white;
+  position: absolute;
+  top: 20px;
+  border-radius: 50%;
+  opacity: 0;
+  height: 154px;
+  background: #fff;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.85;
+    z-index: 1;
+  }
+`
+
 function Header() {
   return (
     <Container>
       <Link href="/">
-        <a><MugShot src="/me.png" /></a>
+        <a>
+          <MugShot src="/me.png" />
+          <Saw src="/saw.svg" />
+        </a>
       </Link>
     </Container>
   )
