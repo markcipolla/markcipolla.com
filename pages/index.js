eb266fd42e03dc0e88b253768c835348dc2ab4e8
Mark Cipolla <mark@markcipolla.com>
2020-08-10 20:46:30 +1000

    Update homepage copy a lil bit

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,33 @@ import Link from 'next/link'
 function Home() {
   return (
     <>
-      <h1>Mark Cipolla</h1>
+      <h1>Hi. I'm Mark.</h1>
 
       <p>
-        Hi. I'm passionate about building things.
+        I'm passionate about <strong>building</strong> things.
       </p>
 
       <p>
-        By trade, I'm a full-stack software engineer, with deep knowledge of HTML/CSS,
-        over a decade in the industry across a number of domains, and skills across
+        By trade, I'm a full-stack software engineer, with deep knowledge in the web, with
+        over a decade in the industry across a number of domains, using skills across
         Javascript / React / Ruby on Rails to deliver large scale projects.
       </p>
 
       <p>
-        Alongside that, I trained as a graphic designer and really like to wear
+        Alongside that, I worked as a visual designer, and really like to wear
         all the hats. 🎩
       </p>
 
       <p>
-        Outside of my day-to-day, I have a startup SaaS
-        called <Link href="/remoteretro.io"><a>RemoteRetro</a></Link> that helps (<em>mostly</em> software) teams
-        run Agile retrospective meetings (especially when the team
-        is remote), and I like to build wooden furniture using fancy joinery techniques. I'm
-        slowly replacing all my furniture with stuff I've made.
+        I'm currently working with <a href="https://cogent.co/">Cogent</a>, and have worked previously
+        with <a href="https://www.clover.com.au/">Clover</a>, <a href="https://theconversation.com/">The Conversation</a> and <a href="https://www.realestate.com.au/">REA</a>.
+      </p>
+
+      <p>
+        Outside of my day-to-day, I have a handy SaaS app
+        called <Link href="/remoteretro.io"><a>RemoteRetro</a></Link> that helps (<em>mostly</em> software-development) teams
+        run Agile retrospective meetings (especially when the team is remote), and I like to build wooden furniture using
+        fancy joinery techniques. I'm slowly replacing all my furniture with stuff I've made.
       </p>
     </>
   )
