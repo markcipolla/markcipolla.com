7b4d78fc9d9f89bb9ca9e5210abed0e6df25afac
Mark Cipolla <mark@markcipolla.com>
2021-01-10 12:24:22 +1100

    Update copy

--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,25 @@ function Home() {
       </p>
 
       <p>
-        By trade, I'm a full-stack software engineer, with deep knowledge in the web, with
-        over a decade in the industry across a number of domains, using skills across
+        By trade, I'm a full-stack software engineer. I have deep knowledge in building web applications,
+        with over a decade in the industry across a number of business domains, and writing
         Javascript / React / Ruby on Rails to deliver large scale projects.
       </p>
 
       <p>
-        Alongside that, I worked as a visual designer, and really like to wear
-        all the hats. 🎩
+        Alongside that, depending on the job, I also sometimes work as a designer (coming from an art and design background at university),
+         and <em>really</em> like to wear all the hats. 🎩
       </p>
 
       <p>
         I'm currently working with <a href="https://cogent.co/">Cogent</a>, and have worked previously
-        with <a href="https://www.clover.com.au/">Clover</a>, <a href="https://theconversation.com/">The Conversation</a> and <a href="https://www.realestate.com.au/">REA</a>.
+        with <a href="https://www.clover.com.au/">Clover</a>, <a href="https://theconversation.com/">The Conversation</a> and <a href="https://www.realestate.com.au/">REA</a>, amongst others.
       </p>
 
       <p>
         Outside of my day-to-day, I have a handy SaaS app
         called <Link href="/remoteretro.io"><a>RemoteRetro</a></Link> that helps (<em>mostly</em> software-development) teams
-        run Agile retrospective meetings (especially when the team is remote), and I like to build wooden furniture using
+        run Agile retrospective meetings (especially when team members are working remote), and I like to build wooden furniture using
         fancy joinery techniques. I'm <a href="https://www.instagram.com/dmaerk/">slowly replacing all my furniture with
         stuff I've made.</a>
       </p>
