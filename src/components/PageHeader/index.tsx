e6060b3323a482631765375e2e77eba4fec83d61
Mark Cipolla <mark@markcipolla.com>
2024-12-13 21:30:40 +1100

    Fix logo/photo to circles

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -9,8 +9,8 @@ export const PageHeader = () => (
   <Block columns={2} className="col-span-5">
     <Link href="/" className="flex flex-row gap-4">
       <div className="relative w-28 rounded-full overflow-hidden border-2 border-black hover:border-green-800 shadow-xl">
-        <Image src={me} alt="Mark Cipolla" className="w-28"/>
-        <Image src={saw} alt="Logo" className="w-28 transition-opacity opacity-0 hover:opacity-80 absolute top-0 left-0" />
+        <Image src={me} alt="Mark Cipolla" className="w-28 h-28"/>
+        <Image src={saw} alt="Logo" className="w-28 h-28 transition-opacity opacity-0 hover:opacity-80 absolute top-0 left-0" />
       </div>
       <h1 className="max-w-[240px] text-7xl font-thin leading-[56px] hover:text-green-800 font-heading uppercase">
         Mark C<span className="tracking-[-.045em]">ipol</span>la
