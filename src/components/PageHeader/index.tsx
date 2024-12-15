3c370566d74e46086779e5a1ab1f9bde05f5e45f
Mark Cipolla <mark@markcipolla.com>
2024-12-16 10:15:44 +1100

    bump textsize

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -12,7 +12,7 @@ export const PageHeader = () => (
         <Image src={me} alt="Mark Cipolla" className="w-20 h-20 md:w-28 md:h-28"/>
         <Image src={saw} alt="Logo" className="w-20 h-20 md:w-28 md:h-28 transition-opacity opacity-0 hover:opacity-80 absolute top-0 left-0" />
       </div>
-      <h1 className="max-w-[140px] md:max-w-[240px] text-5xl md:text-7xl font-thin leading-[38px] md:leading-[56px] hover:text-green-800 font-heading uppercase">
+      <h1 className="max-w-[140px] md:max-w-[240px] text-6xl md:text-7xl font-thin leading-[48px] md:leading-[56px] hover:text-green-800 font-heading uppercase">
         Mark C<span className="tracking-[-.045em]">ipol</span>la
       </h1>
     </Link>
