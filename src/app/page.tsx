d8b37521c4c5836a291a61db5bc0ee7ed3550242
Mark Cipolla <mark@markcipolla.com>
2025-02-06 15:26:24 +1100

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import converter from "number-to-words";
-import {
-  Section,
-  Block,
-  Header,
-  ExternalLink,
-  InternalLink,
-} from "@/components";
+import { Section, Block, Header, ExternalLink, InternalLink } from "@/components";
 import React from "react";
 
 interface Company {
@@ -52,13 +46,7 @@ const slugify = (str: string) =>
     .replace(/[\s_-]+/g, "-")
     .replace(/^-+|-+$/g, "");
 
-const CommaSeparate = ({
-  companies,
-  separator = ", ",
-}: {
-  companies: Company[];
-  separator?: string;
-}) => {
+const CommaSeparate = ({ companies, separator = ", " }: { companies: Company[]; separator?: string }) => {
   return companies.map((company, index) => [
     index > 0 && separator,
     index == companies.length - 1 && "and ",
@@ -71,21 +59,16 @@ const CommaSeparate = ({
 const yearStarted = 2008;
 
 export default function Home() {
-  const yearsExperience = converter.toWords(new Date().getFullYear() - yearStarted)
+  const yearsExperience = converter.toWords(new Date().getFullYear() - yearStarted);
   return (
     <>
       <Section>
         <Block columns={3}>
           <p>I&lsquo;m passionate about building things.</p>
+          <p>I&lsquo;m a full-stack, product-thinking software engineer with an art and graphic design background.</p>
           <p>
-            I&lsquo;m a full-stack, product-thinking software engineer with an
-            art and graphic design background.
-          </p>
-          <p>
-            I have extensive knowledge and experience in building web
-            applications, with over {yearsExperience} years in
-            the industry across a number of business domains, and successfully
-            delivering large scale projects to millions of users.
+            I have extensive knowledge and experience in building web applications, with over {yearsExperience} years in the industry across a number of business domains, and successfully delivering
+            large scale projects to millions of users.
           </p>
         </Block>
       </Section>
@@ -94,30 +77,18 @@ export default function Home() {
         <Header label="Work" />
         <Block columns={3}>
           <p>
-            I&lsquo;m currently working with{" "}
-            <ExternalLink href="https://carbonplus.earth">
-              Carbon Plus
-            </ExternalLink>{" "}
-            on the{" "}
-            <ExternalLink href="https://planr.gov.au">
-              Platform for Land and Nature Repair
-            </ExternalLink>{" "}
-            for the{" "}
-            <ExternalLink href="https://www.dcceew.gov.au/">
-              Department of Climate Change, Energy, the Environment and Water
-            </ExternalLink>{" "}
-            and{" "}
-            <ExternalLink href="https://www.anu.edu.au/">
-              Australian National University
-            </ExternalLink>
-            .
+            I&lsquo;m currently working with <ExternalLink href="https://carbonplus.earth">Carbon Plus</ExternalLink> on the{" "}
+            <ExternalLink href="https://planr.gov.au">Platform for Land and Nature Repair</ExternalLink> for the{" "}
+            <ExternalLink href="https://www.dcceew.gov.au/">Department of Climate Change, Energy, the Environment and Water</ExternalLink> and{" "}
+            <ExternalLink href="https://www.anu.edu.au/">Australian National University</ExternalLink>.
           </p>
           <p>
-            I have worked previously with{" "}
-            <CommaSeparate companies={previousCompanies} />.
+            I have worked previously with <CommaSeparate companies={previousCompanies} />.
           </p>
 
-          <p>Read my full <InternalLink href="/cv">curriculum vitæ</InternalLink>.</p>
+          <p>
+            Read my full <InternalLink href="/cv">curriculum vitæ</InternalLink>.
+          </p>
         </Block>
       </Section>
 
@@ -125,35 +96,14 @@ export default function Home() {
         <Header label="Technical capabilities" />
         <Block columns={3}>
           <dl className="grid grid-cols-3">
-            <Technology
-              category="Frontend"
-              technologies="Deep knowledge of HTML/CSS, React, Typescript / Javascript, Tailwind, Material Design, Formik & Yup, Svelte, jQuery"
-            />
-            <Technology
-              category="Backend"
-              technologies="Ruby, Node, Prisma, .Net, Kotlin"
-            />
-            <Technology
-              category="Design"
-              technologies="Figma, Sketch, Adobe Illustrator & Photoshop"
-            />
+            <Technology category="Frontend" technologies="Deep knowledge of HTML/CSS, React, Typescript / Javascript, Tailwind, Material Design, Formik & Yup, Svelte, jQuery" />
+            <Technology category="Backend" technologies="Ruby, Node, Prisma, .Net, Kotlin" />
+            <Technology category="Design" technologies="Figma, Sketch, Adobe Illustrator & Photoshop" />
             <Technology category="Databases" technologies="PostgreSQL, mySQL" />
-            <Technology
-              category="Mapping"
-              technologies="Leaflet, Geoman, Google Maps"
-            />
-            <Technology
-              category="Testing"
-              technologies="RSpec, Playwright, Cypress"
-            />
-            <Technology
-              category="DevOps"
-              technologies="Github CI, Buildkite, Jenkins, CircleCI"
-            />
-            <Technology
-              category="Containers"
-              technologies="Docker, Docker Composer"
-            />
+            <Technology category="Mapping" technologies="Leaflet, Geoman, Google Maps" />
+            <Technology category="Testing" technologies="RSpec, Playwright, Cypress" />
+            <Technology category="DevOps" technologies="Github CI, Buildkite, Jenkins, CircleCI" />
+            <Technology category="Containers" technologies="Docker, Docker Composer" />
           </dl>
         </Block>
       </Section>
@@ -163,15 +113,12 @@ export default function Home() {
 
         <Block columns={3}>
           <p>
-            <InternalLink href="/remoteretro">RemoteRetro</InternalLink> is a
-            small, profitable SaaS product that helps (mostly software
-            development) teams run better Agile retrospective meetings
+            <InternalLink href="/remoteretro">RemoteRetro</InternalLink> is a small, profitable SaaS product that helps (mostly software development) teams run better Agile retrospective meetings
             (especially when team members are working remote).
           </p>
           <p>
-            <InternalLink href="/grizzlyguts">Grizzly Guts</InternalLink> is an
-            early-stage project to help people with dietary restrictions find
-            restaurants, cafes and shops that cater to their needs.
+            <InternalLink href="/grizzlyguts">Grizzly Guts</InternalLink> is an early-stage project to help people with dietary restrictions find restaurants, cafes and shops that cater to their
+            needs.
           </p>
         </Block>
       </Section>
@@ -179,13 +126,7 @@ export default function Home() {
   );
 }
 
-const Technology = ({
-  category,
-  technologies,
-}: {
-  category: string;
-  technologies: string;
-}) => (
+const Technology = ({ category, technologies }: { category: string; technologies: string }) => (
   <>
     <dt className="font-bold">{category}</dt>
     <dd className="col-span-2">{technologies}</dd>
