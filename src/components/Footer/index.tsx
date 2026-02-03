5111f215a2f15533342107e5800e7347918ff4d4
Mark Cipolla <mark@markcipolla.com>
2026-02-03 14:03:31 +1100

    Add footer email link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Block, ExternalLink, Section } from "@/components";
+import email from "./email.svg";
 import github from "./github.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
@@ -7,6 +8,7 @@ export const Footer = () => (
   <Section>
     <Block columns={3} className="col-start-3">
       <ul className="flex gap-12">
+        <li><ExternalLink href="mailto:mark@markcipolla.com"><Image width={60} src={email} alt="Email" /></ExternalLink></li>
         <li><ExternalLink href="https://github.com/markcipolla"><Image width={40} src={github} alt="GitHub" /></ExternalLink></li>
         <li><ExternalLink href="https://www.linkedin.com/in/mark-cipolla"><Image width={40} src={linkedin} alt="LinkedIn" /></ExternalLink></li>
         <li><ExternalLink href="https://www.instagram.com/dmaerk/"><Image width={40} src={instagram} alt="Instagram" /></ExternalLink></li>
